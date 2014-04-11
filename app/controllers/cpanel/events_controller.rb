4a4eea47c69df0f3afa3327a7e3f25145c425c79
Allen Wu <allenwu1018@gmail.com>
2014-04-12 01:06:50 +0800

    event CRUD

--- app/controllers/cpanel/events_controller.rb
+++ app/controllers/cpanel/events_controller.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 class Cpanel::EventsController < Cpanel::ApplicationController
   def index
     @events = Event.all
@@ -17,12 +18,31 @@ class Cpanel::EventsController < Cpanel::ApplicationController
 
   def create
     @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to cpanel_events_path, :notice => '创建活动成功'
+    else
+      redirect_to cpanel_events_path, :alert => '创建活动失败'
+    end
   end
 
   def update
+    @event = Event.find(params[:id])
+
+    if @event.update_attributes(event_params)
+      redirect_to cpanel_events_path, :notice => '更新活动成功'
+    else
+      redirect_to cpanel_events_path, :alert => '更新活动失败'
+    end
   end
 
   def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      redirect_to cpanel_events_path, :notice => '删除活动成功'
+    else
+      redirect_to cpanel_events_path, :alert => '删除活动失败'
+    end
   end
 
   private
