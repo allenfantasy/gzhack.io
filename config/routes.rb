7548ae8ed8606c0d1a7ee077ffcbd9e77bce0203
Martin <hongzeqin@gmail.com>
2014-04-14 23:31:56 +0800

    fix 404 when visit /en or /zh-CN

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Gzhack::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'home#index'
-
   scope '(:locale)', locale: /en|zh-CN/ do
+    # You can have the root of your site routed with "root"
+    root 'home#index'
+
     get '/event' => 'home#detail'
     get '/about' => 'home#about'
     get '/signup' => 'home#signup'
