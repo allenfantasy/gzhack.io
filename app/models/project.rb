78e19a7ca94da52f5c8f526844b44c9e489ad0bf
Allen Wu <allenwu1018@gmail.com>
2014-04-27 15:28:40 +0800

    demo file is optional

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ActiveRecord::Base
   validates_presence_of :name, :team
-  validates :demo, :presence => true, :associated => true
+  #validates :demo, :presence => true, :associated => true
 
   has_one :demo, :class_name => "ProjectAttachment"
   accepts_nested_attributes_for :demo
