cf6a0f01c6fbd40173ccdb72be467c32803e9179
Allen Wu <allenwu1018@gmail.com>
2014-04-30 14:11:49 +0800

    cpanel project CRUD

--- app/controllers/cpanel/projects_controller.rb
+++ app/controllers/cpanel/projects_controller.rb
@@ -19,9 +19,26 @@ class Cpanel::ProjectsController < Cpanel::ApplicationController
   end
 
   def new
+    @project = Project.new
   end
 
   def create
+    params[:project][:team] = params[:project][:team].values.join(" ")
+    @project = Project.new(project_params)
+
+    fail_msg = t 'activerecord.errors.models.project.failure'
+    succ_msg = t 'activerecord.errors.models.project.success'
+
+    begin
+      @attachment = @project.build_demo(:file => params[:demo][:file][0]) unless params[:demo].nil?
+      @project.save!
+      flash[:success] = succ_msg
+      redirect_to cpanel_projects_path
+    rescue ActiveRecord::RecordInvalid => e
+      flash[:alert] = "#{fail_msg}<br>#{e.record.errors.full_messages.join('<br >')}"
+      @project.build_demo
+      render "new"
+    end
   end
 
   def edit
@@ -30,5 +47,10 @@ class Cpanel::ProjectsController < Cpanel::ApplicationController
 
   def update
     @project = Project.find(params[:id])
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name, :url, :team, :description, :demo_attributes => [:id, :project_id, :file])
   end
 end
