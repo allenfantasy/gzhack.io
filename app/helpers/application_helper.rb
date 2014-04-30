673daa517e6059b48800231605ef5d00501dbbd0
Allen Wu <allenwu1018@gmail.com>
2014-04-30 15:01:43 +0800

    activate projects path

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def language_nav
     content_tag :li do
       if default_locale?
-        link_to "English", :locale => 'en'
+        link_to "EN", :locale => 'en'
       else
         link_to "中文", :locale => 'zh-CN'
       end
